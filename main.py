7488f04b9ddfd8432d6bb3f0ddca4ef489998a8e
Konrad Bledowski <32736495+kbledows@users.noreply.github.com>
2022-10-01 23:40:14 -0400

    main file

---
+++ main.py
@@ -0,0 +1,12 @@
+### Poker Helper Begin Game ###
+# This program is designed to help you play poker.
+# It will tell you what cards you have and what cards you need to win.
+
+# Ideas: Count number of rounds played, and at the end of the session save the number of rounds played, profit (+/-) to a text file. 
+
+def main():
+    print("Welcome to PokerBot 1.0.0")
+
+
+def get_hand():
+    print("Please enter your hand")
