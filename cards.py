df0c24e56504129431186151856310777eb22719
Konrad <32736495+kbledows@users.noreply.github.com>
2022-10-15 17:42:14 -0400

    Add ASCII card printing

--- cards.py
+++ cards.py
@@ -1,9 +1,30 @@
 # cards
 
-class Card:
+class Card(object):
+    card_values = {
+        'A': 11,  # value of the ace is high until it needs to be low
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        'T': 10,
+        'J': 10,
+        'Q': 10,
+        'K': 10
+    }
+
     def __init__(self, value, suit):
+        """
+        :param suit: The face of the card, e.g. Spade or Diamond
+        :param rank: The value of the card, e.g 3 or King
+        """
         self.value = value
         self.suit = suit
+        self.points = self.card_values[value]
 
     def setValue(self, value):
         self.value = value
@@ -16,6 +37,9 @@ class Card:
 
     def getSuit(self):
         return self.suit
+
+    def getPoints(self):
+        return self.points
 
     def printCard(self):
         print("This card is a(n) %s of %s", self.value, self.suit)
--- main.py
+++ main.py
@@ -6,6 +6,13 @@
 
 from cards import Card
 import sys
+import itertools
+import os
+
+cards = ['2s', '2h', '2d', '2c', '3s', '3h', '3d', '3c', '4s', '4h', '4d', '4c', '5s', '5h', '5d', '5c', '6s', '6h', '6d', '6c', '7s', '7h', '7d', '7c', '8s',
+         '8h', '8d', '8c', '9s', '9h', '9d', '9c', 'Ts', 'Th', 'Td', 'Tc', 'Js', 'Jh', 'Jd', 'Jc', 'Qs', 'Qh', 'Qd', 'Qc', 'Ks', 'Kh', 'Kd', 'Kc', 'As', 'Ah', 'Ad', 'Ac']
+
+possible_hands = itertools.combinations(cards, 5)
 
 STier = ['AAo', 'AKs', 'AQs', 'AJs', 'ATs', 'A9s',
          'A8s', 'A7s', 'A6s', 'A5s', 'A4s', 'A3s', 'A2s', 'AKo',
@@ -27,7 +34,7 @@ DTier = ['Q4s', 'Q3s', 'Q2s', 'J6s', 'J5s', 'J4s', 'J3s', 'J2s', 'T5s', 'T4s', '
 
 def poker_hand():
     print("You have just been dealt a new hand. Please enter your cards using the following format:")
-    print("Ten of Clubs == [TC]")
+    print("Ten of Clubs == [Tc]")
     print("Possible card values: 2 3 4 5 6 7 8 9 T J Q K A")
     print("Possible suits: Spades (s), Hearts (h), Clubs (c), Diamonds (d)")
     card1 = input("Enter your first card's value and suit >")
@@ -37,16 +44,15 @@ def poker_hand():
     player_hand1 = ""
     player_hand2 = ""
     suited = "o"
+    os.system('clear')
     print("##################################")
-    if firstCard.suit == secondCard.suit:
-        suited = "s"
-        print("Your cards are suited. Watch for a possible flush draw.")
     player_hand1 += firstCard.value
     player_hand1 += secondCard.value
     player_hand1 += suited
     player_hand2 += secondCard.value
     player_hand2 += firstCard.value
     player_hand2 += suited
+    print_cards([firstCard, secondCard])
     if (player_hand1 in STier) or (player_hand2 in STier):
         print("Your hand is amazing! [S Tier]")
         print("Suggested play: aggressive preflop raise")
@@ -63,6 +69,68 @@ def poker_hand():
     else:
         print("Your hand is trash!")
         print("Suggested play: fold immediately.")
+    if firstCard.suit == secondCard.suit:
+        suited = "s"
+        print("NOTE: Your cards are suited! Watch for a possible flush draw.")
+    print("##################################")
+    print("Pre-flop turn. What would you like to do?")
+    print("1. Call/Raise")
+    print("2. Fold")
+    choice = input(" >")
+    if choice == "1":
+        print("You have decided to keep playing.")
+        # Player now enters 3 flop cards for calculation
+    elif choice == "2":
+        print("You have folded. Thank you for using PokerBot")
+        # update stats and return to main menu
+
+
+def pre_flop():
+    print("Pre-flop turn. What would you like to do?")
+    print("1. Call/Raise")
+    print("2. Fold")
+    choice = input(" >")
+    if choice == "1":
+        print("You have decided to keep playing.")
+        # Player now enters 3 flop cards for calculation
+    elif choice == "2":
+        print("You have decided to fold your hand. Thank you for using PokerBot")
+        # update stats and return to main menu
+
+
+def print_cards(cards):
+    suits_name = ['S', 'D', 'H', 'C']
+    suits_symbols = ['♠', '♦', '♥', '♣']
+    lines = [[] for i in range(9)]
+    for index, card in enumerate(cards):
+        # "King" should be "K" and "10" should still be "10"
+        if card.value == 'T':  # ten is the only one who's rank is 2 char long
+            rank = '10'
+            space = ''  # if we write "10" on the card that line will be 1 char to long
+        else:
+            # some have a rank of 'King' this changes that to a simple 'K' ("King" doesn't fit)
+            rank = card.value
+            space = ' '  # no "10", we use a blank space to will the void
+        # get the cards suit in two steps
+        suit = suits_name.index(card.suit)
+        suit = suits_symbols[suit]
+
+        # add the individual card on a line by line basis
+        lines[0].append('┌─────────┐')
+        # use two {} one for char, one for space or char
+        lines[1].append('│{}{}       │'.format(rank, space))
+        lines[2].append('│         │')
+        lines[3].append('│         │')
+        lines[4].append('│    {}    │'.format(suit))
+        lines[5].append('│         │')
+        lines[6].append('│         │')
+        lines[7].append('│       {}{}│'.format(space, rank))
+        lines[8].append('└─────────┘')
+
+    result = []
+    for index, line in enumerate(lines):
+        result.append(''.join(lines[index]))
+    print('\n'.join(result))
 
 
 def main():
@@ -73,6 +141,12 @@ def main():
     print("1. Play a game")
     print("2. View statistics")
     print("3. Quit")
+    # test_card_1 = Card('4', 'D')
+    # test_card_2 = Card('A', 'C')
+    # test_card_3 = Card('J', 'S')
+    # test_card_4 = Card('T', 'H')
+
+    # print_cards([test_card_1, test_card_2, test_card_3, test_card_4])
     choice = input(" > ")
     if choice == "1":
         print("Starting game...")
